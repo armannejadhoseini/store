153cb09facba0f800218e18e1f7fe8bb2ac4c2f3
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-02-01 11:51:17 +0330

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require('express');
 //express app
 const app = express();
 
+//middleware
+app.use(express.json());
+
 //listen for requests
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port ' + server.address().port)
-})
+});
 
 app.get('/', (req, res) => {
-    res.send('home')
-})
+    res.send('home');
+});
 
 app.get('/about/', (req, res) => {
-    res.send('about')
-})
+    res.send('about');
+});
